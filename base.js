205231868a39aef5cc015d1e3c58ebeb524d4b75
shi-yc <shi.yc@feesoft.com>
2017-06-29 15:37:53 +0800

    add: 增加鼠标悬停停止转动

--- base.js
+++ base.js
@@ -1,8 +1,9 @@
 	var centerx = window.innerWidth / 2;
 	var centery = window.innerHeight / 2;
 	var timeA = time1 = time2 = time3 = time4 = time5 = time5 = time6 = time7 = 0;
+	var timeout = false;
 
-	function name(nameIn, time, r) {
+	function travel(star, time, r) {
 		if (time == timeA) {
 			timeA += 0.1;
 		} else if (time == time1) {
@@ -23,17 +24,44 @@
 		var rad = (2 * Math.PI / 360) * 6 * time;
 		var waterx = centerx + Math.sin(rad) * r;
 		var watery = centery - Math.cos(rad) * r;
-		$("." + nameIn).css({
+		$("." + star).css({
 			"left": waterx + 'px',
 			"top": watery + 'px'
-		})
-
+		}).mouseover(function() {
+			timeout = true;
+		}).mouseout(function() {
+			timeout = false;
+		});
 	}
-	setInterval(function() { name("water", timeA, 70) }, 1);
-	setInterval(function() { name("gold", time1, 110) }, 5);
-	setInterval(function() { name("earth", time2, 160) }, 10);
-	setInterval(function() { name("fire", time3, 200) }, 15);
-	setInterval(function() { name("jupiter", time4, 260) }, 15);
-	setInterval(function() { name("soil", time5, 340) }, 20);
-	setInterval(function() { name("uranus", time6, 410) }, 25);
-	setInterval(function() { name("neptune", time7, 470) }, 30);
+	setInterval(function() {
+		if (timeout) return;
+		travel("water", timeA, 70)
+	}, 1);
+	setInterval(function() {
+		if (timeout) return;
+		travel("gold", time1, 110)
+	}, 5);
+	setInterval(function() {
+		if (timeout) return;
+		travel("earth", time2, 160)
+	}, 10);
+	setInterval(function() {
+		if (timeout) return;
+		travel("fire", time3, 200)
+	}, 15);
+	setInterval(function() {
+		if (timeout) return;
+		travel("jupiter", time4, 260)
+	}, 15);
+	setInterval(function() {
+		if (timeout) return;
+		travel("soil", time5, 340)
+	}, 20);
+	setInterval(function() {
+		if (timeout) return;
+		travel("uranus", time6, 410)
+	}, 25);
+	setInterval(function() {
+		if (timeout) return;
+		travel("neptune", time7, 470)
+	}, 30);
