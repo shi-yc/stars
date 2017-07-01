36e004cb314814ef38d074b7a46e19d0a20c3133
shi-yc <shi.yc@feesoft.com>
2017-07-01 11:54:56 +0800

    从新规划

--- base.js
+++ base.js
@@ -1,12 +1,10 @@
 	var centerx = window.innerWidth / 2;
 	var centery = window.innerHeight / 2;
-	var timeA = time1 = time2 = time3 = time4 = time5 = time5 = time6 = time7 = 0;
+	var time1 = time2 = time3 = time4 = time5 = time5 = time6 = time7 =time8= 0;
 	var timeout = false;
 
 	function travel(star, time, r) {
-		if (time == timeA) {
-			timeA += 0.1;
-		} else if (time == time1) {
+		if (time == time1) {
 			time1 += 0.1;
 		} else if (time == time2) {
 			time2 += 0.1;
@@ -18,50 +16,60 @@
 			time5 += 0.1;
 		} else if (time == time6) {
 			time6 += 0.1;
-		} else {
+		} else if (time == time7) {
 			time7 += 0.1;
+		} else {
+			time8 += 0.1;
 		}
 		var rad = (2 * Math.PI / 360) * 6 * time;
-		var waterx = centerx + Math.sin(rad) * r;
-		var watery = centery - Math.cos(rad) * r;
+		var x = centerx + Math.sin(rad) * r;
+		var y = centery - Math.cos(rad) * r;
 		$("." + star).css({
-			"left": waterx + 'px',
-			"top": watery + 'px'
+			"left": x + 'px',
+			"top": y + 'px'
 		}).mouseover(function() {
 			timeout = true;
+			$("span" + "." + star+"-t").prop('hidden', false);
 		}).mouseout(function() {
 			timeout = false;
+			$("span" + "." + star+"-t").prop('hidden', true);
 		});
+		$(".sun").mousemove(function(event) {
+			$("span.sun-t").prop('hidden', false);
+		}).mouseout(function(event) {
+			$("span.sun-t").prop('hidden', true);
+		});
+
 	}
 	setInterval(function() {
 		if (timeout) return;
-		travel("water", timeA, 70)
-	}, 1);
+		travel("water", time1, 70)
+	}, 4.5);
 	setInterval(function() {
 		if (timeout) return;
-		travel("gold", time1, 110)
-	}, 5);
+		travel("gold", time2, 110)
+	}, 45);
 	setInterval(function() {
 		if (timeout) return;
-		travel("earth", time2, 160)
-	}, 10);
-	setInterval(function() {
-		if (timeout) return;
-		travel("fire", time3, 200)
-	}, 15);
-	setInterval(function() {
-		if (timeout) return;
-		travel("jupiter", time4, 260)
-	}, 15);
-	setInterval(function() {
-		if (timeout) return;
-		travel("soil", time5, 340)
-	}, 20);
-	setInterval(function() {
-		if (timeout) return;
-		travel("uranus", time6, 410)
-	}, 25);
-	setInterval(function() {
-		if (timeout) return;
-		travel("neptune", time7, 470)
+		travel("earth", time3, 160)
 	}, 30);
+	setInterval(function() {
+		if (timeout) return;
+		travel("fire", time4, 200)
+	}, 60);
+	setInterval(function() {
+		if (timeout) return;
+		travel("jupiter", time5, 260)
+	}, 360);
+	setInterval(function() {
+		if (timeout) return;
+		travel("soil", time6, 340)
+	}, 900);
+	setInterval(function() {
+		if (timeout) return;
+		travel("uranus", time7, 410)
+	}, 2520);
+	setInterval(function() {
+		if (timeout) return;
+		travel("neptune", time8, 470)
+	}, 4820);
